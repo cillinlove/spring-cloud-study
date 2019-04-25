0f8d65f15a2be455bc9308ab0059546e19beaaca
cillin <zhibin.ma@sirun.net>
2019-04-25 16:34:45 +0800

    spring-cloud-study

---
+++ microservice-provider-user/src/main/java/com/cillin/cloud/study/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.cillin.cloud.study.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cillin.cloud.study.entity.User;
+
+/**
+ * @author zhouli
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
